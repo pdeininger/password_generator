34776e238ce648c94caa446b31c86730f8633c70
Pam Deininger <pameladeininger@gmail.com>
2019-11-23 17:48:54 -0500

    updated

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ function generate(){
     let complexity = document.getElementById("slider").value;
 
     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-    var confirmUpper = confirm("Would you like to include an upper case letter?")
-    var specialCharacters = confirm("/!", "/@", "/#", "/$", "/%", "/^", "/&", "/*", "/(","/)");
-    var numbers = confirm("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
-    var lowerCase = confirm("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
-    var upperCase = confirm("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+    var upper = ["Would you like to include an upper case letter?"]
+    var specialCharacters = ["/!", "/@", "/#", "/$", "/%", "/^", "/&", "/*", "/(","/)"];
+    var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-
+    confirmUpper.addEventlistener("click", function(event) {
+        event.preventDefault();
+        if(event.target.matches("button")) {
+            var item = document.createElement("div");
+            item.textContent = numbers[event.target.parentElement.id]
+        }
+    })
 
     let password ="";
 
